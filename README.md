279fbc4a2e1be9bf0d00269b1a386dac91e77b26
Stanislav Frolov <stanislav.frolov@dfki.de>
2024-10-22 12:26:55 +0200

    Add code and readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# spotdiffusion
-Official Pytorch Implementation for "SpotDiffusion: A Fast Approach For Seamless Panorama Generation Over Time" (WACV 2025) https://spotdiffusion.github.io/
+# SpotDiffusion (WACV 2025)
+Official Pytorch Implementation for "SpotDiffusion: A Fast Approach For Seamless Panorama Generation Over Time"
+
+[![arXiv](https://img.shields.io/badge/Link_to-Project_Page-blue)](https://spotdiffusion.github.io) 
+[![arXiv](https://img.shields.io/badge/arXiv_Paper-SpotDiffusion-b31b1b.svg)](https://arxiv.org/abs/2302.08113)
+
+[//]: # (### Abstract)
+>Generating high-resolution images with generative models has recently been made widely accessible by leveraging diffusion models pre-trained on large-scale datasets. Various techniques, such as MultiDiffusion and SyncDiffusion, have further pushed image generation beyond training resolutions, i.e., from square images to panorama, by merging multiple overlapping diffusion paths or employing gradient descent to maintain perceptual coherence. However, these methods suffer from significant computational inefficiencies due to generating and averaging numerous predictions, which is required in practice to produce high-quality and seamless images. This work addresses this limitation and presents a novel approach that eliminates the need to generate and average numerous overlapping denoising predictions. Our method shifts non-overlapping denoising windows over time, ensuring that seams in one timestep are corrected in the next. This results in coherent, high-resolution images with fewer overall steps. We demonstrate the effectiveness of our approach through qualitative and quantitative evaluations, comparing it with MultiDiffusion, SyncDiffusion, and StitchDiffusion. Our method offers several key benefits, including improved computational efficiency and faster inference times while producing comparable or better image quality.
+
+# Run
+```
+git clone https://github.com/stanifrolov/spotdiffusion.git
+pip install -r requirements.txt
+python panorama.py
+```
+
+# Citation
+If you find our work useful in your research, please consider citing:
+
+```bibtex
+@article{frolov2024spotdiffusion,
+  title={SpotDiffusion: A Fast Approach For Seamless Panorama Generation Over Time},
+  author={Frolov, Stanislav and Moser, Brian B and Dengel, Andreas},
+  journal={arXiv preprint arXiv:2407.15507},
+  year={2024}
+}
+```
+
+# Acknowledgements
+This code is based on the official implementation of [MultiDiffusion](https://github.com/omerbt/MultiDiffusion/).
+Thanks to the authors for releasing their codebase.
---
+++ panorama.py
@@ -0,0 +1,168 @@
+from transformers import CLIPTextModel, CLIPTokenizer, logging
+from diffusers import AutoencoderKL, UNet2DConditionModel, DDIMScheduler
+
+# suppress partial model loading warning
+logging.set_verbosity_error()
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+import argparse
+from tqdm import tqdm
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = True
+
+
+def get_views(panorama_height, panorama_width, window_size=64, stride=8):
+    panorama_height /= 8
+    panorama_width /= 8
+    num_blocks_height = (panorama_height - window_size) // stride + 1
+    num_blocks_width = (panorama_width - window_size) // stride + 1
+    total_num_blocks = int(num_blocks_height * num_blocks_width)
+    views = []
+    for i in range(total_num_blocks):
+        h_start = int((i // num_blocks_width) * stride)
+        h_end = h_start + window_size
+        w_start = int((i % num_blocks_width) * stride)
+        w_end = w_start + window_size
+        views.append((h_start, h_end, w_start, w_end))
+    return views
+
+
+class SpotDiffusion(nn.Module):
+    def __init__(self, device, sd_version='2.0', hf_key=None):
+        super().__init__()
+
+        self.device = device
+        self.sd_version = sd_version
+
+        print(f'[INFO] loading stable diffusion...')
+        if hf_key is not None:
+            print(f'[INFO] using hugging face custom model key: {hf_key}')
+            model_key = hf_key
+        elif self.sd_version == '2.1':
+            model_key = "stabilityai/stable-diffusion-2-1-base"
+        elif self.sd_version == '2.0':
+            model_key = "stabilityai/stable-diffusion-2-base"
+        elif self.sd_version == '1.5':
+            model_key = "runwayml/stable-diffusion-v1-5"
+        else:
+            raise ValueError(f'Stable-diffusion version {self.sd_version} not supported.')
+
+        # Create model
+        self.vae = AutoencoderKL.from_pretrained(model_key, subfolder="vae").to(self.device)
+        self.tokenizer = CLIPTokenizer.from_pretrained(model_key, subfolder="tokenizer")
+        self.text_encoder = CLIPTextModel.from_pretrained(model_key, subfolder="text_encoder").to(self.device)
+        self.unet = UNet2DConditionModel.from_pretrained(model_key, subfolder="unet").to(self.device)
+
+        self.scheduler = DDIMScheduler.from_pretrained(model_key, subfolder="scheduler")
+
+        print(f'[INFO] loaded stable diffusion!')
+
+    @torch.no_grad()
+    def get_text_embeds(self, prompt, negative_prompt):
+        # prompt, negative_prompt: [str]
+
+        # Tokenize text and get embeddings
+        text_input = self.tokenizer(prompt, padding='max_length', max_length=self.tokenizer.model_max_length,
+                                    truncation=True, return_tensors='pt')
+        text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
+
+        # Do the same for unconditional embeddings
+        uncond_input = self.tokenizer(negative_prompt, padding='max_length', max_length=self.tokenizer.model_max_length,
+                                      return_tensors='pt')
+
+        uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
+
+        # Cat for final embeddings
+        text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+        return text_embeddings
+
+    @torch.no_grad()
+    def decode_latents(self, latents):
+        latents = 1 / 0.18215 * latents
+        imgs = self.vae.decode(latents).sample
+        imgs = (imgs / 2 + 0.5).clamp(0, 1)
+        return imgs
+
+    @torch.no_grad()
+    def text2panorama(self, prompts, negative_prompts='', height=512, width=2048, num_inference_steps=50,
+                      guidance_scale=7.5):
+
+        if isinstance(prompts, str):
+            prompts = [prompts]
+
+        if isinstance(negative_prompts, str):
+            negative_prompts = [negative_prompts]
+
+        # Prompts -> text embeds
+        text_embeds = self.get_text_embeds(prompts, negative_prompts)  # [2, 77, 768]
+
+        # Define panorama grid and get views
+        latent = torch.randn((1, self.unet.in_channels, height // 8, width // 8), device=self.device)
+        # window_size == stride == 64 means non-overlapping views
+        views = get_views(height, width, stride=64)
+        value = torch.zeros_like(latent)
+
+        self.scheduler.set_timesteps(num_inference_steps)
+
+        with torch.autocast('cuda'):
+            for i, t in enumerate(tqdm(self.scheduler.timesteps)):
+                value.zero_()
+
+                for h_start, h_end, w_start, w_end in views:
+                    # TODO we can support batches, and pass multiple views at once to the unet
+                    latent_view = latent[:, :, h_start:h_end, w_start:w_end]
+
+                    # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+                    latent_model_input = torch.cat([latent_view] * 2)
+
+                    # predict the noise residual
+                    noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeds)['sample']
+
+                    # perform guidance
+                    noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
+                    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_cond - noise_pred_uncond)
+
+                    # compute the denoising step with the reference model
+                    latents_view_denoised = self.scheduler.step(noise_pred, t, latent_view)['prev_sample']
+                    value[:, :, h_start:h_end, w_start:w_end] += latents_view_denoised
+
+                # apply the SpotDiffusion shift, efficiently implemented via horizontal duplication and random crop
+                latent = torch.cat((value, value), -1)
+                crop_idx = torch.randint(0, width // 8, (1,)).item()
+                latent = latent[:, :, :, crop_idx:width // 8 + crop_idx]
+
+        # Img latents -> imgs
+        imgs = self.decode_latents(latent)
+        img = T.ToPILImage()(imgs[0].cpu())
+        return img
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prompt', type=str, default='a photo of the dolomites')
+    parser.add_argument('--negative', type=str, default='')
+    parser.add_argument('--sd_version', type=str, default='2.0', choices=['1.5', '2.0'],
+                        help="stable diffusion version")
+    parser.add_argument('--H', type=int, default=512)
+    parser.add_argument('--W', type=int, default=4096)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--steps', type=int, default=50)
+    parser.add_argument('--outfile', type=str, default='out.png')
+    opt = parser.parse_args()
+
+    seed_everything(opt.seed)
+
+    device = torch.device('cuda')
+
+    sd = SpotDiffusion(device, opt.sd_version)
+
+    img = sd.text2panorama(opt.prompt, opt.negative, opt.H, opt.W, opt.steps)
+
+    # save image
+    img.save(opt.outfile)
